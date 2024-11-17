323dd663697f56749fb7e0ade3271ce260ac10fe
dor893 <dor.benartzi@postpost.runi.ac.il>
2024-11-17 20:44:45 +0200

    exercie 4 - solved

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,27 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    
+		int input = Integer.parseInt(args[0]);
+
+		double sum = 0;
+
+		for(int i = 0; i < input; i++) {
+
+			double rationalNumberor = (double)1 / ((2 * i) + 1);
+
+				if(i % 2 == 0) {
+
+					sum += rationalNumberor;
+
+				} else {
+
+					sum -= rationalNumberor;
+				}
+		}
+	System.out.println("pi according to Java: " + (4 * sum));	
+
+	System.out.println("pi, approximated: " + Math.PI);
+
 	}
 }
