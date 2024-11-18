a7093c573909ce5af0379518ea03ee297b7ce777
dor893 <dor.benartzi@postpost.runi.ac.il>
2024-11-19 00:14:45 +0200

    exercise 5 - solved

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,86 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    
+		int seed = Integer.parseInt(args[0]);
+
+		String mood = args[1]; 
+
+		int steps = 0;
+
+		for (int i = 1; i <= seed; i++) {  
+
+			steps = 0;
+
+			int num = i;
+
+			steps++;
+
+			if(mood.equals("v")){
+
+				System.out.print(num + " ");
+			}
+
+			if(i == 1) {
+
+				   if(num % 2 == 0) {
+
+				    num = num / 2;
+
+			       } else {
+
+				    num = (3 * num) + 1;
+			       }
+
+			   if(mood.equals("v")){
+
+				System.out.print(num + " " );
+
+	           }
+
+			   steps++;
+
+			   if(num == 1) {
+
+				   break;
+
+			   }
+
+		    }
+
+			while (num != 1) {
+
+			    if(num % 2 == 0) {
+
+				    num = num / 2;
+
+			    } else {
+
+				    num = (3 * num) + 1;
+			    }
+
+			if(mood.equals("v")){
+
+				System.out.print(num + " " );
+
+			}
+
+			steps++;
+
+		    }
+
+		 if(mood.equals("v")){
+
+			 System.out.println("(" + steps + ")");
+
+		 }
+	    }
+
+	if(mood.equals("c")){
+
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+
+	}
+
 	}
 }
